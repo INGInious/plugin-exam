fe77f12e59b22a355595d76f4ff726e20ad984c4
Anthony Gégo <anthonygego@hotmail.com>
2020-06-08 14:15:37 +0200

    Add feature to join a webcam snapshot into the submission input

--- inginious-exam/__init__.py
+++ inginious-exam/__init__.py
@@ -5,6 +5,7 @@
 
 """ A demo plugin that adds a page """
 
+import logging
 import hashlib
 import os
 import bisect
@@ -14,7 +15,7 @@ import web
 
 from inginious.frontend.accessible_time import AccessibleTime
 from inginious.frontend.pages.course_admin.utils import INGIniousAdminPage
-from inginious.frontend.pages.utils import INGIniousAuthPage
+from inginious.frontend.pages.utils import INGIniousAuthPage, INGIniousPage
 
 PATH_TO_PLUGIN = os.path.abspath(os.path.dirname(__file__))
 
@@ -44,6 +45,7 @@ class ExamAdminPage(INGIniousAdminPage):
             course_content["exam_password"] = input_data["password"]
             course_content["seb_hash"] = input_data["sebhash"]
             course_content["exam_active"] = True if input_data["active"] == "true" else False
+            course_content["exam_webcam"] = True if input_data["webcam"] == "true" else False
             self.course_factory.update_course_descriptor_content(courseid, course_content)
         elif input_data.get("action", "") == "finalize":
             if input_data["username"] == "*":
@@ -89,8 +91,7 @@ class ExamAdminPage(INGIniousAdminPage):
 
         tpl = self.template_helper.get_custom_renderer(PATH_TO_PLUGIN).admin
 
-        return tpl(PATH_TO_PLUGIN, course, course_content.get("exam_active", False),
-                   course_content.get("exam_password", ""), course_content.get("seb_hash", ""), mysebhash, thesebhash, user_data, errors, saved)
+        return tpl(PATH_TO_PLUGIN, course, course_content, mysebhash, thesebhash, user_data, errors, saved)
 
 
 class ExamPage(INGIniousAuthPage):
@@ -190,20 +191,46 @@ def course_menu(course, template_helper):
         return ""
 
 
-class FakeCSSPage(object):
-    def GET(self):
-        if web.ctx.environ.get("HTTP_X_SAFEEXAMBROWSER_REQUESTHASH", ""):
-            return "#logoff_button {display:none;} a.mailto {display:none;}"
+def css_header():
+    return "/plugins/exam/static/exam-style.css" \
+        if web.ctx.environ.get("HTTP_X_SAFEEXAMBROWSER_REQUESTHASH", "") else None
 
-        return ""
+
+class StaticMockPage(object):
+    # TODO: Replace by shared static middleware and let webserver serve the files
+    def GET(self, path):
+        if not os.path.abspath(PATH_TO_PLUGIN) in os.path.abspath(os.path.join(PATH_TO_PLUGIN, path)):
+            raise web.notfound()
+
+        try:
+            with open(os.path.join(PATH_TO_PLUGIN, "static", path), 'rb') as file:
+                return file.read()
+        except:
+            raise web.notfound()
+
+    def POST(self, path):
+        return self.GET(path)
 
 class SebQuitPage(object):
     def GET(self):
         return "<html><body><p><a href='" + web.ctx.homepath +"/seb-quit'>Click here to exit</a></p></body></html>"
 
+def task_menu(course, task, template_helper):
+    course_content = course.get_descriptor()
+    if course_content.get("exam_active", False) and course_content.get("exam_webcam", False):
+        return str(template_helper.get_custom_renderer(PATH_TO_PLUGIN, False).task_menu())
+    else:
+        return ""
+
+
+def new_submission(submission, task_input, course_factory):
+    course_content = course_factory.get_course_descriptor_content(submission["courseid"])
+    if course_content.get("exam_active", False) and course_content.get("exam_webcam", False) and not task_input.get("@picture", ""):
+        raise Exception("A webcam snapshot is required to submit, but your webcam couldn't be accessed.")
+
 def init(plugin_manager, course_factory, client, config):
     """ Init the plugin """
-
+    plugin_manager.add_page('/plugins/exam/static/(.+)', StaticMockPage)
     plugin_manager.add_page("/admin/([^/]+)/exam", ExamAdminPage)
     plugin_manager.add_hook('course_admin_menu', add_admin_menu)
     plugin_manager.add_hook('course_accessibility', lambda course, default: course_accessibility(course, default,
@@ -212,17 +239,22 @@ def init(plugin_manager, course_factory, client, config):
                                                                                                  plugin_manager.get_user_manager()))
     plugin_manager.add_hook('course_allow_unregister', lambda course, default: False if course.get_descriptor().get("exam_active", False) else default)
     plugin_manager.add_hook('course_menu', course_menu)
+    plugin_manager.add_hook('task_menu', task_menu)
     plugin_manager.add_page("/exam/([^/]+)", ExamPage)
-    plugin_manager.add_page("/exam-style.css", FakeCSSPage)
-    plugin_manager.add_hook('css', lambda: "/exam-style.css")
+
+    plugin_manager.add_hook('css', css_header)
+    plugin_manager.add_hook('css', lambda : "/plugins/exam/static/webcamjs.css")
     plugin_manager.add_page('/seb-quit', SebQuitPage)
     add_hook(plugin_manager, 'javascript_header', lambda: javascript_header(plugin_manager.get_database(),
                                                                                                  plugin_manager.get_user_manager(), course_factory))
 
+    add_hook(plugin_manager, 'javascript_header',
+             lambda: "https://cdnjs.cloudflare.com/ajax/libs/webcamjs/1.0.26/webcam.min.js")
     add_hook(plugin_manager, 'main_menu', lambda template_helper: main_menu(template_helper,
                                                                                       plugin_manager.get_database(),
                                                                                       plugin_manager.get_user_manager(),
                                                                                       course_factory))
+    add_hook(plugin_manager, 'new_submission', lambda submission, inputdata: new_submission(submission, inputdata, course_factory))
 
 
 def add_hook(plugin_manager, name, callback):
